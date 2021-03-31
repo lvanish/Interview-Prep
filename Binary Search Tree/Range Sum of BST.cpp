784f2b0cb6a54beda9300fd0dad677226f24c5e4
Lvanish Koul <lvanishkoul@gmail.com>
2021-03-31 21:59:26 +0530

    Create Range Sum of BST.cpp

---
+++ Binary Search Tree/Range Sum of BST.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void findSum(TreeNode* root, int low, int high, int &sum){
+        if(!root) return;
+        if(root->val >= low and root->val <= high){
+            sum += root->val;
+        }
+        if(low < root->val){
+          findSum(root->left, low, high, sum);
+        }
+        if(high > root->val){
+          findSum(root->right, low, high, sum);
+        }
+        
+    }
+    
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        findSum(root, low, high, sum);
+        return sum;
+    }
+};
